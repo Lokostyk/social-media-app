0d17d6803588a4b481ac3c4337c276ca2d123a1d
Lokostyk <lokostyk12@gmail.com>
2021-09-01 23:11:10 +0200

    Added post creation

--- src/components/addPost.js
+++ src/components/addPost.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
 
 export default function AddPost() {
+    window.addEventListener("click",()=>{
+        setView("")
+    })
+
     const [postContent,setPostContent] = useState("Create your post here")
     const [view,setView] = useState("")
+
+    function expand(e) {
+        e.stopPropagation()
+        setView("fullView")
+        postContent === "Create your post here" ? setPostContent("") : console.log()
+    }
     return (
-        <div className="postAdd">
-            <label className={`postLabel ${view}`}>
+        <div className={`postAdd ${view}`}>
+            <label className="postLabel" onClick={expand}>
                 <p>Add new post</p>
-                <textarea value={postContent} onChange={(e)=>setPostContent(e.target.value)} onFocus={()=>setView("fullView")} onBlur={()=>setView("")}/>
+                <textarea className={`postTextarea ${view}`} value={postContent} onChange={(e)=>setPostContent(e.target.value)}/>
+                <div className="postBtns">
+                    <button className="rBtn">Delete</button>
+                    <button className="rBtn">Photo</button>
+                    <button className="rBtn">Add</button>
+                </div>
             </label>
         </div>
     )
