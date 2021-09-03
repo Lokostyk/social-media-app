4e2bf579c9353939e04ebeb54d6c7c1db917df8e
Lokostyk <lokostyk12@gmail.com>
2021-09-03 21:41:33 +0200

    Small fixes

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import firebase from 'firebase/app'
 
 export default function Posts() {
+    const [posts,setPosts] = useState("")
+    
+    if(posts === ""){renderSeven()}
+    function renderSeven(){
+        const data = []
+        
+        firebase.database().ref("posts/").limitToLast(7).get().then((snapshot)=> {
+            for(let i in snapshot.val()){
+                data.push(snapshot.val()[i])
+            }
+            setPosts(data)
+        })
+    }
+    function render(items){
+        if(items !== ""){
+            return items.map((e)=> {return <div>{e.content}</div>}).reverse()
+        }
+    }
     return (
         <div>
-            yooo
+            {render(posts)}
+            <button onClick={()=>setPosts("")}>YOO</button>
         </div>
     )
 }
