00a2d7967053fa7d7223596c901aaa175e364b09
Lokostyk <lokostyk12@gmail.com>
2021-09-15 23:04:45 +0200

    Infinite scroll added

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,13 +1,14 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect, useCallback , useRef} from 'react'
 import firebase from 'firebase/app'
 
-export default function Posts(props) {
+export default function Posts(props) {    
     const [posts,setPosts] = useState([])
     const lastPost = 100
 
+    useEffect(renderPosts,[])
     function renderPosts(){
         const loadPosts = new Array()
-        firebase.database().ref("posts/").limitToLast(3).get().then((snapshot)=> {
+        firebase.database().ref("posts/").orderByChild("date").get().then((snapshot)=> {
             if(snapshot.val() !== null){
                 let index = 0
                 for(let i in snapshot.val()){
@@ -26,6 +27,18 @@ export default function Posts(props) {
             }
         })
     }
+    //Function will load more if last post is on screen
+    function loadMorePosts(node) {
+        if(!node) return
+        const observer = new IntersectionObserver(entries=>{
+            if(entries[0].isIntersecting){
+                renderPosts()
+                observer.disconnect()
+            }
+        })
+        observer.observe(node)
+    }
+
     function expandPostSettings(e) {
         const list = e.target.parentElement.nextElementSibling
         list.classList.toggle("expandSettings")
@@ -47,15 +60,28 @@ export default function Posts(props) {
                 setPosts(posts.filter((item)=>{return item.date !== date}))
         })
     }
-    window.addEventListener("scroll",(e)=>{
-        const documentHeight = document.querySelector("#defaultMiddleContent").getBoundingClientRect()
-        console.log(documentHeight)
-    })
     return (
-        <React.Fragment>
-            {posts.map(item => 
-                {return (
-                    <div className={`post`} key={item.date}>
+        <>
+            {posts.map((item,index) => 
+                {return (posts.length - 1 === index?
+                    <div className="post" ref={loadMorePosts} key={item.date}>
+                        <div className="postBox">
+                            <p className="topUserInfo">{item.userName + " "}{item.userSurname}</p>
+                            <p className="postTextContent" style={item.photoUrl?{textAlign: "center"}:{}}>{item.content}</p>
+                            <img src={item.photoUrl} className={item.photoUrl ? "postImage":"noDisplay"} />
+                            <div onClick={(e)=>e.stopPropagation()} className={`${props.loggedIn && (firebase.auth().currentUser.uid === item.userId)? "showPostSettings" : ""} settings`}>
+                                <button onClick={(e)=>expandPostSettings(e)}>
+                                    <img src="pictures/gear_icon.png" />
+                                </button>
+                                <ul id="list">
+                                    <li><button onClick={()=> editPost(item.date)}>Edit</button></li>
+                                    <li><button onClick={()=> deletePost(item.date)}>Delete</button></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className="post" key={item.date}>
                         <div className="postBox">
                             <p className="topUserInfo">{item.userName + " "}{item.userSurname}</p>
                             <p className="postTextContent" style={item.photoUrl?{textAlign: "center"}:{}}>{item.content}</p>
@@ -72,7 +98,6 @@ export default function Posts(props) {
                         </div>
                     </div>
                 )})}
-            <button onClick={renderPosts}>YOO</button>
-        </React.Fragment>
+        </>
     )
 }
